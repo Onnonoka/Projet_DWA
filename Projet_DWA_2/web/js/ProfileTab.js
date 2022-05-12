33e3ace262903820a86e61c652e591eefbc49306
ZiQuwi <axelcaz@hotmail.fr>
2022-05-12 12:47:23 +0200

    modif_profil

--- Projet_DWA_2/web/js/ProfileTab.js
+++ Projet_DWA_2/web/js/ProfileTab.js
@@ -16,23 +16,27 @@ class ProfileTab {
         this.container = document.getElementById("main-content");
         let content = "";
         content += `<div class="profile">
-            <div class="pseudo">${this.player.pseudo}</div>
+            <div class="tetepseudo">
+                <div class="pseudo">${this.player.pseudo}</div>
+                <div class="button">
+                    ${ this.changeProfil ? '<button id="confirmProfil">Confirmer le changement</button>' : '<button id="modifProfil">Modifier le profil</button>'}
+                </div>
+            </div>
+            <hr />
             <div class="info">
                 <div>Mdp : ${this.changeProfil ? '<input type="text" id="MdpProfil" value=' + this.player.mdp + '>' : this.player.mdp}</div>
                 <div>Sexe : ${this.changeProfil ? '<input type="text" id="SexProfil" value=' + this.player.sexe + '>' : this.player.sexe}</div>
                 <div>Ville : ${this.changeProfil ? '<input type="text" id="VilleProfil" value=' + this.player.ville + '>' : this.player.ville}</div>
                 <div>Age : ${this.changeProfil ? '<input type="text" id="AgeProfil" value=' + this.player.age + '>' : this.player.age}</div>
             </div>
+            <hr />
             <div class="data">
                 <div>Nombre de victoire : ${this.player.win}</div>
                 <div>Nombre de partie jouer : ${this.player.played}</div>
                 <div>Ratio de victoire : ${isNaN(this.player.win / this.player.played)? 0 : this.player.win / this.player.played}</div>
                 <div>Nombre moyen de jeton charge : ${this.player.average1}</div>
                 <div>Nombre moyen de jeton decharge : ${this.player.average2}</div>
-            </div>
-            <div class="button">
-                ${ this.changeProfil ? '<button id="confirmProfil">Confirmer le changement</button>' : '<button id="modifProfil">Modifier le profil</button>'}
-            </div>
+            </div> 
         </div>`;
         this.container.innerHTML = content;
         this.updateEventListener();
